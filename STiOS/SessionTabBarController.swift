e7253e95c0b295147d8ddcfefa95150be2585fdc
Kurtis Gibson <kurtisgibsonmusic@gmail.com>
2017-01-20 10:14:40 -0600

    Gave titles to tabs in tab bar controller, rearranged the order.

---
+++ STiOS/SessionTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTabBarController.swift
+//  STiOS
+//
+//  Created by Kurtis Gibson on 2017-01-19.
+//  Copyright © 2017 ECE 4600 G09. All rights reserved.
+//
+
+import UIKit
+
+class SessionTabBarController: UITabBarController, Dismissable {
+    
+    weak var dismissalDelegate: DismissalDelegate?
+    
+    }
